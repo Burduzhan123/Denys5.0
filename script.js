5c944a8529a009450c78802084abc458a095a22e
Denys <denisburduzan@gmail.com>
2023-12-05 18:49:38 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const condition = 1
+
+if(condition === 2022){
+    console.log('You are 1 year old');
+} else if (condition === 2018){
+    console.log('You are 5 years old');
+}else if (condition === 2000){
+    console.log ('You are 23 years old');
+}else{
+    console.log('smth else')
+}
