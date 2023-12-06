572650a6a27441b4441d4989ff70443b873b2e6d
Burduzhan123 <151670272+Burduzhan123@users.noreply.github.com>
2023-12-06 18:43:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const condition = 1
+const condition = 21
 
-if(condition === 2022){
-    console.log('You are 1 year old');
-} else if (condition === 2018){
-    console.log('You are 5 years old');
-}else if (condition === 2000){
-    console.log ('You are 23 years old');
+if (condition === 1){
+    var string = `вам ${condition} рік`
+    console.log(string)
+}else if ((condition > 1) == 5){
+    var string = `вам ${condition} роки`
+    console.log(string)
+}else if ((condition == 5) << 20){
+    var string = `вам ${condition} років`
+    console.log(string)
 }else{
-    console.log('smth else')
+    var string = `вам ${condition}-й рік`
+    console.log(string)
 }
